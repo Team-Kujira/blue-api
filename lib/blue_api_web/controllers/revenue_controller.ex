2eb460301c4593a8e841497038736e566398e4ae
codehans <94654388+codehans@users.noreply.github.com>
2024-07-31 18:26:42 +0100

    days lookback

--- lib/blue_api_web/controllers/revenue_controller.ex
+++ lib/blue_api_web/controllers/revenue_controller.ex
@@ -3,7 +3,8 @@ defmodule BlueApiWeb.RevenueController do
 
   def index(conn, params) do
     with {:ok, to} <- get_to(params),
-         {:ok, from} <- get_from(to, params),
+         {days, ""} <- get_days(params),
+         {:ok, from} <- get_from(to, days, params),
          {:ok, result} <- compile_range(from, to) do
       total = Map.values(result) |> Enum.reduce(&sum_rewards/2)
       json(conn, %{days: result, total: total})
@@ -13,8 +14,11 @@ defmodule BlueApiWeb.RevenueController do
   defp get_to(%{"to" => x}), do: NaiveDateTime.from_iso8601(x)
   defp get_to(%{}), do: {:ok, NaiveDateTime.beginning_of_day(NaiveDateTime.local_now())}
 
-  defp get_from(_, %{"from" => x}), do: NaiveDateTime.from_iso8601(x)
-  defp get_from(to, %{}), do: {:ok, NaiveDateTime.add(to, -7, :day)}
+  defp get_from(_, _, %{"from" => x}), do: NaiveDateTime.from_iso8601(x)
+  defp get_from(to, days, %{}), do: {:ok, NaiveDateTime.add(to, -days, :day)}
+
+  defp get_days(%{"days" => x}), do: Integer.parse(x)
+  defp get_days(%{}), do: {7, ""}
 
   defp compile_range(from, to) do
     {:ok,
